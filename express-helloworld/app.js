06db7093c7e92820c5592177caa450b38c8704b1
shandrade <41609330+shandrade@users.noreply.github.com>
2020-07-12 01:08:11 -0300

    Update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,11 +1,16 @@
 var express = require('express');
-app = express();
+var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello, World!\n');
+});
+
+app.get('/mars', function (req, res) {
+  res.send('Hello, Mars!\n');
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('App escutando na porta 8080!');
 });
 
+module.exports = app;
